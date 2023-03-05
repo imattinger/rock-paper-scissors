a8c8efcaef97acc45512d3666cc32a51e1a6fb01
Ingrid <17310165+imattinger@users.noreply.github.com>
2023-03-04 20:29:19 -0800

    Add function game()

--- rock-paper-scissors-game.js
+++ rock-paper-scissors-game.js
@@ -9,17 +9,46 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toUpperCase();
     if (playerSelection == computerSelection) {
-        return `Both players choose ${computerSelection}. It's a tie!`
+        console.log(`Both players choose ${computerSelection}. It's a tie!`);
+        return 0;
     }
     if (playerSelection == 'ROCK' && computerSelection == 'PAPER' ||
         playerSelection == 'PAPER' && computerSelection == 'SCISSORS' ||
         playerSelection == 'SCISSORS' && computerSelection == 'ROCK') {
-            return `You lose! ${computerSelection} beats ${playerSelection}.`;
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}.`);
+            return -1;
     }
-    return `You win! ${playerSelection} beats ${computerSelection}.`
+    console.log(`You win! ${playerSelection} beats ${computerSelection}.`);
+    return 1;
 }
 
-// For testing
-const playerMove = 'rock';
-const computerMove = getComputerChoice();
-console.log(playRound(playerMove, computerMove));
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    while (playerScore < 5 && computerScore < 5) {
+        let playerSelection = prompt("Choose rock, paper, or scissors.");
+        playerSelection = playerSelection.toUpperCase();
+        if (playerSelection != 'ROCK' && playerSelection != 'PAPER' &&
+        playerSelection != 'PAPER') {
+            console.log("Invalid input. You get to use rock.")
+            playerSelection = 'ROCK';
+        }
+        computerSelection = getComputerChoice();
+        let result = playRound(playerSelection, computerSelection);
+        if (result < 0) {
+            computerScore += 1;
+        }
+        if (result > 0) {
+            playerScore += 1;
+        }
+        console.log(`Player score: ${playerScore}\tComputer score: ${computerScore}`);
+    }
+    if (playerScore == 5) {
+        alert("You win!")
+    }
+    else {
+        alert("Computer wins!")
+    }
+}
+
+game();

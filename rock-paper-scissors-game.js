e1505d88ca20d10c41df372128fa75297e696ecd
Ingrid <17310165+imattinger@users.noreply.github.com>
2023-03-04 20:11:05 -0800

    Add function playRound()

--- rock-paper-scissors-game.js
+++ rock-paper-scissors-game.js
@@ -6,4 +6,20 @@ function getComputerChoice() {
     return MOVES[choice];
 }
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+    if (playerSelection == computerSelection) {
+        return `Both players choose ${computerSelection}. It's a tie!`
+    }
+    if (playerSelection == 'ROCK' && computerSelection == 'PAPER' ||
+        playerSelection == 'PAPER' && computerSelection == 'SCISSORS' ||
+        playerSelection == 'SCISSORS' && computerSelection == 'ROCK') {
+            return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    }
+    return `You win! ${playerSelection} beats ${computerSelection}.`
+}
 
+// For testing
+const playerMove = 'rock';
+const computerMove = getComputerChoice();
+console.log(playRound(playerMove, computerMove));
